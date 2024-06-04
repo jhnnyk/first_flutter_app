13dd9bf1af291dd423c1a3a32ad558554e7433d2
John Knotts <jhnnyk@gmail.com>
2024-06-04 11:15:59 -0600

    adding dice and buttons and some styling

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
+
+void rollDice() {}
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.startColor, this.endColor, {super.key});
@@ -20,9 +23,38 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-2.png',
-          width: 200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const StyledText('Sup homie?'),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/dice-2.png',
+                  width: 150,
+                ),
+                Image.asset(
+                  'assets/images/dice-5.png',
+                  width: 150,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: rollDice,
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: const Color.fromARGB(255, 58, 72, 221),
+              ),
+              child: const Text('Roll the Dice'),
+            )
+          ],
         ),
       ),
     );
